8539b7581f24dc8436dfa46755391bb73ec5519d
Alof Bronco <130389810+AlofBronco@users.noreply.github.com>
2025-06-12 18:53:59 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
-<<<<<<< filters
-import './js/artist-modal';
-import './js/filters';
-import '/js/artists/artists';
-=======
 import '/js/artist-modal';
 import '/js/artists/artists';
 import './js/feedback-modal';
 import './js/header-menu';
 import './js/hero.js';
->>>>>>> main
+import './js/filters';
