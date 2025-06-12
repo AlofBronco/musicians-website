a1281882ea18abe53a4d1e2e5dc942a0a6bdf8e8
AlofBronco <130389810+AlofBronco@users.noreply.github.com>
2025-06-12 15:41:23 +0000

    Deploying to gh-pages from @ AlofBronco/musicians-website@66808127d00af3f2cde77356061114e8d7654bf6 🚀

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import{a as b}from"./assets/vendor-BbYF_8OW.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))o(s);new MutationObserver(s=>{for(const r of s)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&o(i)}).observe(document,{childList:!0,subtree:!0});function n(s){const r={};return s.integrity&&(r.integrity=s.integrity),s.referrerPolicy&&(r.referrerPolicy=s.referrerPolicy),s.crossOrigin==="use-credentials"?r.credentials="include":s.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function o(s){if(s.ep)return;s.ep=!0;const r=n(s);fetch(s.href,r)}})();const m=document.querySelector(".description-wrapper"),p=document.querySelector(".modal-title"),g=document.querySelector(".albums-list"),k=document.querySelector(".loader"),C=document.querySelector(".artists-gallery"),c=document.querySelector(".backdrop"),$=document.querySelector(".modal-close-button"),L=document.querySelector(".albums-title");let w;function N(){k.classList.remove("visually-hidden")}function Y(){k.classList.add("visually-hidden")}async function U(e){return(await b.get(`https://sound-wave.b.goit.study/api/artists/${e}`)).data}async function G(e){m.innerHTML="",g.innerHTML="",p.innerHTML="",L.innerHTML="",N();try{const t=await U(e);L.innerHTML="Albums",$.addEventListener("click",d),c.addEventListener("click",S),document.addEventListener("keydown",E),W(t),X(t)}catch(t){console.error("Error loading artist data:",t)}finally{Y()}}function _(e){const t=Math.floor(e/1e3),n=Math.floor(t/60),o=t%60;return`${n}:${o.toString().padStart(2,"0")}`}function F({strTrack:e,intDuration:t,movie:n}){return`
+import{a as k}from"./assets/vendor-BbYF_8OW.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))o(s);new MutationObserver(s=>{for(const r of s)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&o(i)}).observe(document,{childList:!0,subtree:!0});function n(s){const r={};return s.integrity&&(r.integrity=s.integrity),s.referrerPolicy&&(r.referrerPolicy=s.referrerPolicy),s.crossOrigin==="use-credentials"?r.credentials="include":s.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function o(s){if(s.ep)return;s.ep=!0;const r=n(s);fetch(s.href,r)}})();const m=document.querySelector(".description-wrapper"),p=document.querySelector(".modal-title"),f=document.querySelector(".albums-list"),$=document.querySelector(".loader"),N=document.querySelector(".artists-gallery"),c=document.querySelector(".backdrop"),S=document.querySelector(".modal-close-button"),v=document.querySelector(".albums-title");let T;function C(){$.classList.remove("visually-hidden")}function Y(){$.classList.add("visually-hidden")}async function U(t){return(await k.get(`https://sound-wave.b.goit.study/api/artists/${t}`)).data}async function G(t){m.innerHTML="",f.innerHTML="",p.innerHTML="",v.innerHTML="",C();try{const e=await U(t);v.innerHTML="Albums",S.addEventListener("click",d),c.addEventListener("click",w),document.addEventListener("keydown",M),J(e),X(e)}catch(e){console.error("Error loading artist data:",e)}finally{Y()}}function _(t){const e=Math.floor(t/1e3),n=Math.floor(e/60),o=e%60;return`${n}:${o.toString().padStart(2,"0")}`}function F({strTrack:t,intDuration:e,movie:n}){return`
 <li class="tracks-item">
-              <p class="track-name">${e}</p>
-              <p class="track-length">${_(t)}</p>
+              <p class="track-name">${t}</p>
+              <p class="track-length">${_(e)}</p>
               <div class="track-link-space">
                 ${n?`<a class="track-link" href='${n}' target="_blank" rel="noopener noreferrer"
                   ><svg class="track-link-icon" width="24" height="24">
@@ -11,32 +11,32 @@ import{a as b}from"./assets/vendor-BbYF_8OW.js";(function(){const t=document.cre
                 ></a>`:""} 
               </div>
             </li>
-    `}function R(e){return e.map(F).join("")}function z({tracksList:e}){const t={},n=Math.min(100,e.length);for(let o=0;o<n;o++){const s=e[o],r=s.strAlbum;t[r]||(t[r]=[]),t[r].push(s)}return t}function K(e,t){return`
+    `}function R(t){return t.map(F).join("")}function K({tracksList:t}){const e={},n=Math.min(100,t.length);for(let o=0;o<n;o++){const s=t[o],r=s.strAlbum;e[r]||(e[r]=[]),e[r].push(s)}return e}function V(t,e){return`
         <li class="albums-item">
-          <h5 class="album-title">${e}</h5>
+          <h5 class="album-title">${t}</h5>
           <div class="tracks-heading">
             <p class="tracks-heading-text">Track</p>
             <p class="tracks-heading-text">Time</p>
             <p class="tracks-heading-text">Link</p>
           </div>
           <ul class="tracks">
-          ${R(t[e])}
+          ${R(e[t])}
           </ul>
         </li>
-    `}function V(e){return Object.keys(e).map(t=>K(t,e)).join("")}function W(e){const t=z(e);g&&g.insertAdjacentHTML("beforeend",V(t))}function l(e,t){return`
+    `}function z(t){return Object.keys(t).map(e=>V(e,t)).join("")}function J(t){const e=K(t);f&&f.insertAdjacentHTML("beforeend",z(e))}function l(t,e){return`
     <li class="artist-desc-item">
-      <h4 class="artist-desc-title">${e}</h4>
-      <p class="artist-desc-text">${t}</p>
+      <h4 class="artist-desc-title">${t}</h4>
+      <p class="artist-desc-text">${e}</p>
     </li>
-  `}function J(e){return`
+  `}function Q(t){return`
     <li class="artist-genres-item">
-      <p class="artist-genres-text">${e}</p>
+      <p class="artist-genres-text">${t}</p>
     </li>
-  `}function Q(e){const{intDiedYear:t,intFormedYear:n,intMembers:o,strArtist:s,strArtistThumb:r,strBiographyEN:i,strCountry:u,strGender:D}=e;p&&(p.textContent=s);const I=w;return`
+  `}function W(t){const{intDiedYear:e,intFormedYear:n,intMembers:o,strArtist:s,strArtistThumb:r,strBiographyEN:i,strCountry:u,strGender:D}=t;p&&(p.textContent=s);const I=T;return`
       <img class="modal-img" src="${r}" alt="${s}" />
       <div class="description-text-wrapper">
         <ul class="artist-description">
-          ${l("Years active",`${n} - ${t||"present"}`)}
+          ${l("Years active",`${n} - ${e||"present"}`)}
           ${l("Sex",D)}
           ${l("Members",o)}
           ${l("Country",u)}
@@ -46,18 +46,18 @@ import{a as b}from"./assets/vendor-BbYF_8OW.js";(function(){const t=document.cre
           <p class="artist-desc-text">${i}</p>
         </div>
         <ul class="artist-genres">
-          ${I.map(J).join("")}
+          ${I.map(Q).join("")}
         </ul>
       </div>
-    `}function X(e){m&&m.insertAdjacentHTML("beforeend",Q(e))}C.addEventListener("click",e=>{e.target.nodeName==="BUTTON"&&(c.classList.add("is-open"),document.body.style.overflowY="hidden",w=e.target.dataset.genres.split(","),G(e.target.dataset.id))});function d(){c.classList.remove("is-open"),document.body.style.overflowY="auto",$.removeEventListener("click",d),c.removeEventListener("click",S),document.removeEventListener("keydown",E)}function S(e){e.target===c&&d()}function E(e){e.key==="Escape"&&d()}const Z="https://sound-wave.b.goit.study/api",ee="/artists";function M(e=1){const t={limit:8,page:e};return b.get(`${Z}${ee}`,{params:t}).then(n=>n.data).catch(n=>{throw console.error("Error fetching artists:",n),n})}const te="/musicians-website/img/sprite.svg",y=document.querySelector(".load-more-btn"),T=document.querySelector(".loader-artists");function se({_id:e,genres:t,strArtist:n,strBiographyEN:o,strArtistThumb:s}){return`<li class="artists-item">
+    `}function X(t){m&&m.insertAdjacentHTML("beforeend",W(t))}N.addEventListener("click",t=>{t.target.nodeName==="BUTTON"&&(c.classList.add("is-open"),document.body.style.overflowY="hidden",T=t.target.dataset.genres.split(","),G(t.target.dataset.id))});function d(){c.classList.remove("is-open"),document.body.style.overflowY="auto",S.removeEventListener("click",d),c.removeEventListener("click",w),document.removeEventListener("keydown",M)}function w(t){t.target===c&&d()}function M(t){t.key==="Escape"&&d()}const Z="https://sound-wave.b.goit.study/api",tt="/artists";function E(t=1){const e={limit:8,page:t};return k.get(`${Z}${tt}`,{params:e}).then(n=>n.data).catch(n=>{throw console.error("Error fetching artists:",n),n})}const et="/musicians-website/img/sprite.svg",L=document.querySelector(".load-more-btn"),A=document.querySelector(".loader-artists");function st({_id:t,genres:e,strArtist:n,strBiographyEN:o,strArtistThumb:s}){return`<li class="artists-item">
         <img class="artists-img" src="${s}" alt="${n}" width="288"/>
-        <ul class="artists-genres">${t.map(r=>`<li class="artists-genres-item"><p class="artists-genres-text">${r}</p></li>`).join(`
+        <ul class="artists-genres">${e.map(r=>`<li class="artists-genres-item"><p class="artists-genres-text">${r}</p></li>`).join(`
 `)}</ul>
                   <h3 class="artsits-name">${n}</h3>
         <p class="artists-descr">${o}</p>
-        <button class="learn-btn" type="button" data-id="${e}" data-genres="${t}">Learn More<svg class="learn-more-svg" width="24" height="24">
-        <use href="${te}#icon-triangle-right"></use>
+        <button class="learn-btn" type="button" data-id="${t}" data-genres="${e}">Learn More<svg class="learn-more-svg" width="24" height="24">
+        <use href="${et}#icon-triangle-right"></use>
       </svg></button>
-      </li>`}function A(e){return e.map(se).join(`
-`)}function re(){y.classList.remove("visually-hidden")}function v(){y.classList.add("visually-hidden")}function q(e,t){if(e<t){re();return}else v()}function x(){T.classList.remove("visually-hidden")}function B(){T.classList.add("visually-hidden")}y.addEventListener("click",ne);let a=1,f=null;const j=document.querySelector(".artists-gallery");x();v();M(a).then(e=>{const t=A(e.artists);f=Math.ceil(e.totalArtists/e.limit),j.insertAdjacentHTML("beforeend",t),B(),q(a,f)});function ne(){v(),x(),a++,M(a).then(e=>{const t=A(e.artists);j.insertAdjacentHTML("beforeend",t),B(),q(a,f)})}const h=document.querySelector(".backdrop-header"),O=document.querySelector(".js-toggle-menu"),H=O.querySelector(".menu-icon"),oe=document.querySelector(".js-logo-icon use"),P=()=>{const e=window.innerWidth>=768?"/img/sprite.svg#icon-header-logo":"/img/sprite.svg#icon-header-logo-mobile";oe.setAttribute("href",e)};O.addEventListener("click",()=>{const e=h.classList.toggle("is-open");H.setAttribute("href",e?"/img/sprite.svg#icon-header-close-menu":"/img/sprite.svg#icon-header-open-menu"),document.body.classList.toggle("menu-open",e)});document.querySelectorAll('a[href^="#"]').forEach(e=>{e.addEventListener("click",t=>{t.preventDefault();const n=e.getAttribute("href").substring(1),o=document.getElementById(n);if(o){const r=document.querySelector(".header").offsetHeight,u=o.getBoundingClientRect().top+window.scrollY-r;window.scrollTo({top:u,behavior:"smooth"})}h.classList.contains("is-open")&&(h.classList.remove("is-open"),H.setAttribute("href","/img/sprite.svg#icon-header-open-menu"),document.body.classList.remove("menu-open"))})});window.addEventListener("resize",()=>{P()});window.addEventListener("DOMContentLoaded",()=>{P()});function ie(){const e=document.querySelector("#btn");e&&e.addEventListener("click",()=>{const t=document.querySelector("#artists");t&&t.scrollIntoView({behavior:"smooth",block:"start"})})}document.addEventListener("DOMContentLoaded",()=>{ie()});
+      </li>`}function q(t){return t.map(st).join(`
+`)}function rt(){L.classList.remove("visually-hidden")}function b(){L.classList.add("visually-hidden")}function x(t,e){if(t<e){rt();return}else b()}function B(){A.classList.remove("visually-hidden")}function j(){A.classList.add("visually-hidden")}L.addEventListener("click",nt);let a=1,g=null;const H=document.querySelector(".artists-gallery");B();b();E(a).then(t=>{const e=q(t.artists);g=Math.ceil(t.totalArtists/t.limit),H.insertAdjacentHTML("beforeend",e),j(),x(a,g)});function nt(){b(),B(),a++,E(a).then(t=>{const e=q(t.artists);H.insertAdjacentHTML("beforeend",e),j(),x(a,g)})}const h="/musicians-website/img/sprite.svg",y=document.querySelector(".backdrop-header"),O=document.querySelector(".js-toggle-menu"),P=O.querySelector(".menu-icon");O.addEventListener("click",()=>{const t=y.classList.toggle("is-open");P.setAttribute("href",t?`${h}#icon-header-close-menu`:`${h}#icon-header-open-menu`),document.body.classList.toggle("menu-open",t)});document.querySelectorAll('a[href^="#"]').forEach(t=>{t.addEventListener("click",e=>{e.preventDefault();const n=t.getAttribute("href").substring(1),o=document.getElementById(n);if(o){const r=document.querySelector(".header").offsetHeight,u=o.getBoundingClientRect().top+window.scrollY-r;window.scrollTo({top:u,behavior:"smooth"})}y.classList.contains("is-open")&&(y.classList.remove("is-open"),P.setAttribute("href",`${h}#icon-header-open-menu`),document.body.classList.remove("menu-open"))})});function ot(){const t=document.querySelector("#btn");t&&t.addEventListener("click",()=>{const e=document.querySelector("#artists");e&&e.scrollIntoView({behavior:"smooth",block:"start"})})}document.addEventListener("DOMContentLoaded",()=>{ot()});
 //# sourceMappingURL=index.js.map
